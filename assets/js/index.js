86d1791beb2db666c00ace626882753640a00e18
Jitendra Vishwakarma <jiturajkarma2020@gmail.com>
2023-10-05 17:56:15 +0530

    toggle changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,12 @@
 function hidebar() {
 	let menu = document.getElementById("menus");
+	let full = document.getElementById('mainbar');
 	if (menu.style.display=="none") {
 		menu.style.display="block";
+		full.style.marginLeft = "16%";
 	}
 	else {
 		menu.style.display="none";
+		full.style.marginLeft = "0%";
 	}
 }
