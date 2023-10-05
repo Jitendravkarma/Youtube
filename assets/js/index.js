642c2bc514ed078d4660300a76af231d3d0350e2
Jitendra Vishwakarma <jiturajkarma2020@gmail.com>
2023-10-05 17:02:27 +0530

    UI is done except responsive

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+function hidebar() {
+	let menu = document.getElementById("menus");
+	if (menu.style.display=="none") {
+		menu.style.display="block";
+	}
+	else {
+		menu.style.display="none";
+	}
+}
