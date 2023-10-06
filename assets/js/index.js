96e3fe0293c55fd49134d95a47446c9436c32678
Jitendra Vishwakarma <jiturajkarma2020@gmail.com>
2023-10-06 12:03:58 +0530

    new youtube clone with responsive

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,22 @@
-function hidebar() {
-	let menu = document.getElementById("menus");
-	let full = document.getElementById('mainbar');
-	if (menu.style.display=="none") {
-		menu.style.display="block";
-		full.style.marginLeft = "16%";
+function largbar() {
+	let bar = document.getElementById('sidebar');
+	let main = document.getElementById('content');
+	if (bar.style.display === "none") {
+		bar.style.display ="block";
+		main.style.width ="80%";
 	}
 	else {
-		menu.style.display="none";
-		full.style.marginLeft = "0%";
+		bar.style.display ="none";
+		main.style.width ="100%";
+	}
+}
+function smallbar() {
+	let bar = document.getElementById('sidebar');
+	let main = document.getElementById('content');
+	if (bar.style.left === "0%") {
+		bar.style.left ="-50%";
+	}
+	else {
+		bar.style.left ="0%";
 	}
 }
